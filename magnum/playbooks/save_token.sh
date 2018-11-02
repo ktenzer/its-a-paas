92c7de270b452cdad0e87fe5d407cd2face359bf
Fedora <fedora@bastion.novalocal>
2018-11-02 11:48:16 +0000

    completed playbooks for setting up student workspaces

---
+++ magnum/playbooks/save_token.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in {00..40}; do
+  TOKEN=`kubectl -n student$i describe secret $(kubectl -n student$i get secret  | grep student$i | awk '{print $1}') |grep "token:"`
+  echo $TOKEN >> credentials/student$i/password.txt
+done
